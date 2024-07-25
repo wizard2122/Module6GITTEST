300d7a77d4ac37a6ee53aec91c63171cccbaa156
Yakovlev-Ilya-IT <67857830+wizard2122@users.noreply.github.com>
2024-07-25 18:18:42 +0300

    Изменения в ветке main

--- Assets/NewFolder/Scripts/NewScript2.cs
+++ Assets/NewFolder/Scripts/NewScript2.cs
@@ -7,7 +7,7 @@ public class NewScript2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Изменения в ветке main");
     }
 
     // Update is called once per frame
